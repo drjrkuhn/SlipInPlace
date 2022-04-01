277a35a4f86192acdbeba422ba8800291be47750
Jeffrey Kuhn <drjrkuhn@gmail.com>
2022-04-01 15:34:18 -0400

    Added example program

--- examples/sip_encode_oop/sip_encode_oop.ino
+++ examples/sip_encode_oop/sip_encode_oop.ino
@@ -1,40 +1,52 @@
+/*********************************************************************
+ * \file   sip_encode_oop.ino
+ * \brief  Arduino example of out-of-place and in-place SLIP encoding
+ * 
+ * Uses a human-readable SLIP encoding scheme instead of the standard
+ * octal END and ESC codes.
+ * 
+ * \author Jeffrey Kuhn <jrkuhn@mit.edu>
+ * \date   April 2022
+ *********************************************************************/
+
+#include <Arduino.h>
 #include <slipinplace.h>
 
+// human-readable encoders and decoders
+//                               END  ESC ESCEND ESCESC
 typedef slip::encoder_base<char, '#', '^', 'D', '['> encoder_hr;
 typedef slip::decoder_base<char, '#', '^', 'D', '['> decoder_hr;
 
 String to_escaped_string(const char* buf, size_t size) {
     String out;
-    while(size--) {
-      char c = *(buf++);
-      if (isPrintable(c)) {
-        out.concat(c);
-      } else if (c == '\0') {
-        out.concat('\0');
-      } else {
-        String oct("000");
-        oct += String((int)c,OCT);
-        int len = oct.length();
-        out += oct.substring(len-3,len);
-      }
+    while (size--) {
+        char c = *(buf++);
+        if (isPrintable(c)) {
+            out.concat(c);
+        } else if (c == '\0') {
+            out.concat("\\0");
+        } else {
+            String octstr("000");
+            octstr += String((int)c, OCT);
+            out += '\\';
+            int len = octstr.length();
+            out += octstr.substring(len - 3, len);
+        }
     }
     return out;
 }
 void test_encode_decode(String srcstr, size_t bufsize, bool inplace) {
     char buf[bufsize];
-    const char* src           = srcstr.c_str();
-    size_t srcsize       = srcstr.length();
+    const char* src = srcstr.c_str();
+    size_t srcsize  = srcstr.length();
     memset(buf, '.', bufsize);
     if (inplace) {
         memcpy(buf, src, srcsize);
         src = buf;
     }
-    if (inplace) {
-      Serial.println("___in-place encode and decode___");
-    } else {
-      Serial.println("___out-of-place encode and decode___");
-    }
-    Serial.print("src:  \"");    Serial.print(srcstr);    Serial.print("\"");
+    Serial.print("src:  \"");
+    Serial.print(srcstr);
+    Serial.print("\"");
     size_t est_nencoded = encoder_hr::encoded_size(src, srcsize);
     if (est_nencoded > bufsize) {
         Serial.print("<<< !!Warning!! dsize not big enough to hold encoded string");
@@ -44,10 +56,16 @@ void test_encode_decode(String srcstr, size_t bufsize, bool inplace) {
 
     // encode
     nencoded = encoder_hr::encode(buf, bufsize, src, srcsize);
-    String encstr(buf, nencoded);
+    String encstr;
     if (nencoded < bufsize) buf[nencoded] = '\0';
-    Serial.print("encs: \"");    Serial.print(encstr);    Serial.println("\"");
-    Serial.print("ebuf: [");    Serial.print(to_escaped_string(buf, bufsize));    Serial.println("]");
+    encstr.copy(buf, nencoded);
+    if (nencoded < bufsize) buf[nencoded] = '\0';
+    Serial.print("encs: \"");
+    Serial.print(encstr);
+    Serial.println("\"");
+    Serial.print("ebuf: [");
+    Serial.print(to_escaped_string(buf, bufsize));
+    Serial.println("]");
 
     // decode
     size_t est_ndecoded = decoder_hr::decoded_size(buf, nencoded);
@@ -58,9 +76,15 @@ void test_encode_decode(String srcstr, size_t bufsize, bool inplace) {
         ndecoded = decoder_hr::decode(buf, bufsize, encstr.c_str(), encstr.length());
     }
 
-    String decstr(buf, ndecoded);
-    Serial.print("decs: \"");    Serial.print(decstr);    Serial.println("\"");
-    Serial.print("dbuf: [");    Serial.print(to_escaped_string(buf, bufsize));    Serial.println("]");
+    String decstr;
+    if (ndecoded < bufsize) buf[ndecoded] = '\0';
+    decstr.copy(buf, ndecoded);
+    Serial.print("decs: \"");
+    Serial.print(decstr);
+    Serial.println("\"");
+    Serial.print("dbuf: [");
+    Serial.print(to_escaped_string(buf, bufsize));
+    Serial.println("]");
 
     if (est_nencoded != nencoded) {
         Serial.print("!!Warning!! encoded size estimate:");
@@ -74,34 +98,60 @@ void test_encode_decode(String srcstr, size_t bufsize, bool inplace) {
         Serial.print(" did not match source size:");
         Serial.println(srcsize);
     }
-    if (decstr != srcstr) {
+    if (decstr.compareTo(srcstr) != 0) {
         Serial.println("!!Warning!! decoded string did not match source string");
     }
-    Serial.print(" bufsize:"); Serial.print(bufsize);
-    cout Serial.print( " srcsize:"); Serial.print( srcsize);
-    cout Serial.print( " est_nencoded:"); Serial.print( est_nencoded);
-    cout Serial.print( " nencoded:"); Serial.print( nencoded);
-    cout Serial.print( " est_ndecoded:"); Serial.print( est_ndecoded);
-    cout Serial.print( " ndecoded:"); Serial.print( ndecoded);
-    cout Serial.println();
-    cout Serial.println();
+    Serial.print("       bufsize:");
+    Serial.print(bufsize);
+    Serial.print(" srcsize:");
+    Serial.print(srcsize);
+    Serial.print(" est_nencoded:");
+    Serial.print(est_nencoded);
+    Serial.print(" nencoded:");
+    Serial.print(nencoded);
+    Serial.print(" est_ndecoded:");
+    Serial.print(est_ndecoded);
+    Serial.print(" ndecoded:");
+    Serial.print(ndecoded);
+    Serial.println();
 }
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial)
-    ;
-
-  Serial.println("=== sip_encode_oop ===");
-
+    Serial.begin(9600);
+    while (!Serial)
+        ;
 }
 
 void loop() {
-    String out1 = "Lorus Ipsum";
-    String out2 = "Favius## Rex\\ \\\\#\\##Aeturnum padre##";
+    Serial.println("OUT-OF-PLACE ENCODE AND DECODE");
+    Serial.println("========================================");
+    test_encode_decode(String(""), 40, false);
+    test_encode_decode(String("Lorus"), 40, false);
+    test_encode_decode(String("Lorus#"), 40, false);
+    test_encode_decode(String("Lo^#rus"), 40, false);
+    test_encode_decode(String("Lorus^"), 40, false);
+    test_encode_decode(String("Lorus#"), 40, false);
+    test_encode_decode(String("Lorus^##"), 40, false);
+    test_encode_decode(String("Lo^r#us i^ps#um#"), 40, false);
+    Serial.println("      ____exact fit_____");
+    test_encode_decode(String("Lor^us##"), 12, false);
+    Serial.println("      ___will overrun___");
+    test_encode_decode(String("Lor^us##"), 11, false);
 
-    test_encode_decode(out1, 40, false);
-    test_encode_decode(out2, 40, false);
+    Serial.println("IN-PLACE ENCODE AND DECODE");
+    Serial.println("========================================");
+    test_encode_decode(String(""), 40, true);
+    test_encode_decode(String("Lorus"), 40, true);
+    test_encode_decode(String("Lorus#"), 40, true);
+    test_encode_decode(String("Lo^#rus"), 40, true);
+    test_encode_decode(String("Lorus^"), 40, true);
+    test_encode_decode(String("Lorus#"), 40, true);
+    test_encode_decode(String("Lorus^##"), 40, true);
+    test_encode_decode(String("Lo^r#us i^ps#um#"), 40, true);
+    Serial.println("      ____exact fit_____");
+    test_encode_decode(String("Lor^us##"), 12, true);
+    Serial.println("      ___will overrun___");
+    test_encode_decode(String("Lor^us##"), 11, true);
 
-    delay(1000);
+    delay(10000);
 }
